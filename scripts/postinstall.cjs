79a5d285ffbe5bc4c39694fa9ca5a9146cd8045d
unsync <unsync@example.com>
2024-01-28 16:04:06 +0100

    update

---
+++ scripts/postinstall.cjs
@@ -0,0 +1,19 @@
+const fs = require('fs')
+
+const rootPath = require.main.paths[0].split('node_modules')[0]
+
+// add npm script
+const packageJsonPath = `${rootPath}package.json`
+if(fs.existsSync(packageJsonPath)) {
+    console.info('postinstall: add package.json scripts')
+    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath))
+
+    packageJson.scripts['release:patch'] = 'npm version -m \'release:patch\' patch'
+    packageJson.scripts['release:minor'] = 'npm version -m \'release:minor\' minor'
+    packageJson.scripts['release:major'] = 'npm version -m \'release:major\' major'
+
+    packageJson.scripts['checkdeps'] = 'ncu'
+    packageJson.scripts['checkdeps:apply'] = 'ncu -u'
+
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2))
+}
